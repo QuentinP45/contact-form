6828f2475c44f98ce0593b2cd5eefd12e6a76240
Quentin Picart <quentin.picart@gmail.com>
2019-01-21 19:36:06 +0100

    refactor: add form to homepage from controller

--- src/Company/CompanyBundle/Controller/CompanyController.php
+++ src/Company/CompanyBundle/Controller/CompanyController.php
@@ -3,11 +3,16 @@
 namespace Company\CompanyBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Company\CompanyBundle\Form\ContactType;
 
 class CompanyController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('@CompanyCompany/company/home.html.twig');
+        $form = $this->createForm(ContactType::class);
+
+        return $this->render('@CompanyCompany/company/home.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
