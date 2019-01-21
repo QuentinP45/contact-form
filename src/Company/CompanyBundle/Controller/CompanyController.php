b48c33ccd0398cad685af0c11e26a2b9f0e8fc59
Quentin Picart <quentin.picart@gmail.com>
2019-01-21 17:46:54 +0100

    feat: return home page template

--- src/Company/CompanyBundle/Controller/CompanyController.php
+++ src/Company/CompanyBundle/Controller/CompanyController.php
@@ -2,13 +2,12 @@
 
 namespace Company\CompanyBundle\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class CompanyController extends Controller
 {
     public function indexAction()
     {
-        return new Response("Hello world");
+        return $this->render('@CompanyCompany/company/home.html.twig');
     }
 }
