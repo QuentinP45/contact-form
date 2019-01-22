7cf3ce903eac773a821ad8641bb8e14eadfe7a81
Quentin Picart <quentin.picart@gmail.com>
2019-01-22 08:46:11 +0100

    Feat: add message flash for a submit message

--- src/Company/CompanyBundle/Controller/CompanyController.php
+++ src/Company/CompanyBundle/Controller/CompanyController.php
@@ -3,13 +3,28 @@
 namespace Company\CompanyBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Company\CompanyBundle\Form\ContactType;
 
 class CompanyController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
         $form = $this->createForm(ContactType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $this->addFlash(
+                'info',
+                'Message envoyé, merci !'
+            );
+
+            return $this->render('@CompanyCompany/company/home.html.twig', [
+                'form' => $form->createView(),
+            ]);
+        }
 
         return $this->render('@CompanyCompany/company/home.html.twig', [
             'form' => $form->createView(),
