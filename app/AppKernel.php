7e04ff81b4749939d69d78b328a874bcb7bb87a5
Quentin Picart <quentin.picart@gmail.com>
2019-01-22 10:09:52 +0100

    feat: create admin bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Company\CompanyBundle\CompanyCompanyBundle(),
+            new Company\AdminBundle\CompanyAdminBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
---
+++ src/Company/AdminBundle/CompanyAdminBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Company\AdminBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CompanyAdminBundle extends Bundle
+{
+}
