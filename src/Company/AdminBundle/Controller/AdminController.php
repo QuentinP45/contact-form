239c38ccea7410ce8ecbc86e6bfb1843bfd85d12
Quentin Picart <quentin.picart@gmail.com>
2019-01-22 16:18:32 +0100

    feat: handle checked submission from contact edit form

--- src/Company/AdminBundle/Controller/AdminController.php
+++ src/Company/AdminBundle/Controller/AdminController.php
@@ -3,7 +3,9 @@
 namespace Company\AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Company\CompanyBundle\Entity\Message;
+use Company\CompanyBundle\Form\ContactEditType;
 
 class AdminController extends Controller
 {
@@ -18,14 +20,25 @@ class AdminController extends Controller
         ]);
     }
 
-    public function viewAction($message)
+    public function viewAction($message, Request $request)
     {
         $em = $this->getDoctrine()->getManager();
         
         $message = $em->getRepository(Message::class)->find($message);
 
+        $form = $this->createForm(ContactEditType::class, $message);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $em->persist($message);
+            $em->flush();
+        }
+
         return $this->render('@CompanyAdmin/admin/message.html.twig', [
-            'message' => $message
+            'message' => $message,
+            'form' => $form->createView()
         ]);
     }
 }
