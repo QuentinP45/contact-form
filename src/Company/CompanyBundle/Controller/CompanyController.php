9418f018da02a6d32eaa38799943d49c4119b488
Quentin Picart <quentin.picart@gmail.com>
2019-01-22 09:57:06 +0100

    feat: save messages in database

--- src/Company/CompanyBundle/Controller/CompanyController.php
+++ src/Company/CompanyBundle/Controller/CompanyController.php
@@ -5,16 +5,26 @@ namespace Company\CompanyBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Company\CompanyBundle\Form\ContactType;
+use Company\CompanyBundle\Entity\Message;
 
 class CompanyController extends Controller
 {
     public function indexAction(Request $request)
     {
-        $form = $this->createForm(ContactType::class);
+        $message = new Message();
+
+        $form = $this->createForm(ContactType::class, $message);
 
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+
+            $em = $this->getDoctrine()->getManager();
+
+            $message->setDateCreation(new \Datetime('now'));
+
+            $em->persist($message);
+            $em->flush();
 
             $this->addFlash(
                 'info',
