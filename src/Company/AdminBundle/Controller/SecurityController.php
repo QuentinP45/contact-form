4e257a1d9c5a5b356e08a1f3389d8f58f25d1f8f
Quentin Picart <quentin.picart@gmail.com>
2019-01-23 10:54:49 +0100

    feat: create login action

--- src/Company/AdminBundle/Controller/SecurityController.php
+++ src/Company/AdminBundle/Controller/SecurityController.php
@@ -6,4 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class SecurityController extends Controller
 {
+    public function loginAction()
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('@CompanyAdmin/admin/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ]);
+    }
 }
